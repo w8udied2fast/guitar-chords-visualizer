a6e5cf2c34005990096f14ec5c7243098983162c
Sergei Markidonov <w8udie2often@gmail.com>
2024-11-04 13:32:10 +0200

    Add logic files

--- guitar-chords-visualizer/src/main/java/com/example/GuitarFretboard.java
+++ guitar-chords-visualizer/src/main/java/com/example/GuitarFretboard.java
@@ -14,14 +14,14 @@ public class GuitarFretboard extends Canvas {
         GraphicsContext gc = getGraphicsContext2D();
         gc.clearRect(0, 0, getWidth(), getHeight());
 
-        // гриф гитары
+        // Рисуем гриф гитары
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
         for (int i = 0; i < 6; i++) {
             gc.strokeLine(50, 50 + i * 50, 50 + 200, 50 + i * 50);
         }
 
-        // Расположение пальцев
+        // Рисуем расположение пальцев
         for (int i = 0; i < strings.length; i++) {
             int fret = Integer.parseInt(strings[i]);
             if (fret > 0) {
@@ -29,5 +29,10 @@ public class GuitarFretboard extends Canvas {
                 gc.fillOval(50 + fret * 50 - 10, 50 + i * 50 - 10, 20, 20);
             }
         }
+    }
+
+    public void clear() {
+        GraphicsContext gc = getGraphicsContext2D();
+        gc.clearRect(0, 0, getWidth(), getHeight());
     }
 }
